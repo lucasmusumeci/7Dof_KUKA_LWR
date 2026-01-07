f31d17de72d8b8d3dc848ee1034878a6a5bd3475
lucas.musumeci <lucas.musumeci@etu.umontpellier.fr>
2026-01-07 13:39:56 +0100

    Cmd cinématique avec orientations

--- main.jl
+++ main.jl
@@ -16,35 +16,38 @@ end
 T7=zeros(4,4);
 init_pos()
 
+# --- Constantes ---
 global rob=CreateRobotKukaLwr();
 global θinit=[0.143019, -0.109465, -0.011994, -1.1788, -0.154233, 0.93555, 0.264868];
 global θmin=(pi/180)*[-170, -120, -170, -120, -170, -120, -170];
 global θmax=(pi/180)*[170, 120, 170, 120, 170, 120, 170];
 global ωmax=(pi/180)*[112.5, 112.5, 112.5, 112.5, 180, 112.5, 112.5];
-global pA=[-0.3668, -0.0379, 0.8634];
-global pB=[-0.3668, -0.0379, 0.5];
-global K=1;
-dP = [0.0, 0.0, -0.001];
-dt = 0.05;
+global Pb=[-0.3668, -0.0379, 0.5];
+global Ab=RTL2R(pi/12, pi/6, 0);
+global Kp=1;
+global K0=1;
+global dP = [0.0, 0.0, -0.001];
+global dω = [0.0, 0.0, 0.0];
+global dt = 0.05;
 
-function inverseKinematics(θi, Pf, rob, dP, dt)
+function inverseKinematicsPos(θi, Pf, rob, dP, dt)
     q = θi;
     q_collect = Vector{Vector{Float64}}()
     push!(q_collect, copy(q))
     t0 = time()
     # First time to get inside the while
     Te, MTe = MGD(q, rob);
-    #Ae = Te[1:3,1:3]
     Pe = Te[1:3,4];
     ϵP = Pf .- Pe;
-    #ϵ0 = 0.5*(cross(Ae[1:3,1],Ad[1:3,1]) + cross(Ae[1:3,2],Ad[1:3,2]) + cross(Ae[1:3,3],Ad[1:3,3]));
     i=0;
     while norm(ϵP) > 1e-4 && i < 1000
         # Calcul de dPe
-        ϵP = Pf .- Pe
+        ϵP = Pf .- Pe;
+        dPe = Kp*ϵP .+ dP;
         # Change 3 to 6 to get orientation control as well
-        J = Jacobian(q, rob, Pe)[1:3,:];
-        dq = eloignement_butees_articulaires(J, (K*ϵP .+ dP), q, θmax, θmin, -0.5);
+        J = Jacobian(q, rob, Pe)[1:3, :];
+        dq = LinearAlgebra.pinv(J) * (Kp*ϵP .+ dP)
+        #dq = eloignement_butees_articulaires(J, dPe, q, θmax, θmin, -0.5);
 
         q = q + dq*dt;
         push!(q_collect, copy(q))
@@ -60,13 +63,69 @@ function inverseKinematics(θi, Pf, rob, dP, dt)
         # Recalcul de Pe avec MGDrob
         t0 = time()
         Te, MTe = MGD(q, rob);
-        #Ae = Te[1:3,1:3]
         Pe = Te[1:3,4]
         i=i+1
-
     end
     
     return q_collect
+end
+
+function inverseKinematics(θi, Pf, Af, rob, dP, dω, dt, λ_L=0.1)
+    q = θi;
+    q_collect = Vector{Vector{Float64}}()
+    epsilon0_collect = Vector{Vector{Float64}}()
+    push!(q_collect, copy(q))
+    t0 = time()
+    # First time to get inside the while
+    Te, MTe = MGD(q, rob);
+    Ae = Te[1:3,1:3]
+    Pe = Te[1:3,4];
+    ϵP = Pf .- Pe;
+    ϵ0 = 0.5*(cross(Ae[1:3,1],Af[1:3,1]) + cross(Ae[1:3,2],Af[1:3,2]) + cross(Ae[1:3,3],Af[1:3,3]));
+    push!(epsilon0_collect, copy(ϵ0))
+    i=0;
+    while (norm(ϵP) > 1e-4 || norm(ϵ0) > 1e-2) && i < 1000
+        # Calcul ωe
+        ϵ0 = 0.5*(cross(Ae[1:3,1],Af[1:3,1]) + cross(Ae[1:3,2],Af[1:3,2]) + cross(Ae[1:3,3],Af[1:3,3]))
+        if norm(ϵ0) < 1e-2
+            ωe = dω  # Pas de correction, juste suivre dω
+        else
+            # Calcul normal avec amortissement
+            L = calcul_L(Af, Ae)
+            L_damped = L' * inv(L * L' + λ_L^2 * I(3))  # Car L est mal conditionée donc pinv(L) donne des valeurs énormes
+            ωe = L_damped * (K0 * ϵ0 + L' * dω)
+        end
+        # Calcul de dPe
+        ϵP = Pf .- Pe;
+        dPe = Kp*ϵP .+ dP;
+        # Change 3 to 6 to get orientation control as we ll
+        J = Jacobian(q, rob, Pe)[1:6, :];
+        Ẋ = [dPe; ωe];
+        dq = LinearAlgebra.pinv(J) * Ẋ;
+        #dq = eloignement_butees_articulaires(J, Ẋ, q, θmax, θmin, -0.5);
+
+        q = q + dq*dt;
+        push!(q_collect, copy(q))
+        #t1 = time()     
+        setjointposition(clientID,q,7,0,objectname_kuka)
+        t2 = time()
+        #println("Temps de calcul : $(t1 - t0) secondes")
+        #println("Temps VREP : $(t2 - t1) secondes")
+        #println("Reste à attendre : $(dt-(t2-t0)) secondes")
+        sleep(max(0.0,round(dt-(t2-t0), digits=4)))
+        #println("Itération $i : Position actuelle = $Pe, Erreur = $(norm(Pf-Pe))")
+
+        # Recalcul de Pe avec MGDrob
+        t0 = time()
+        Te, MTe = MGD(q, rob);
+        Ae = Te[1:3,1:3]
+        Pe = Te[1:3,4]
+        ϵ0 = 0.5*(cross(Ae[1:3,1],Af[1:3,1]) + cross(Ae[1:3,2],Af[1:3,2]) + cross(Ae[1:3,3],Af[1:3,3]))
+        push!(epsilon0_collect, copy(ϵ0))
+        i=i+1
+    end
+    
+    return q_collect, epsilon0_collect
 end
 
 function eloignement_butees_articulaires(J, Ẋ,  θ, θmax, θmin, α)
@@ -105,14 +164,21 @@ function chapeau(v)
     return v_chap
 end
 
+# --- Contrôle du robot ---
 # Robot en position initiale
 setjointposition(clientID,θinit,7,0,objectname_kuka)
-print("Robot en position : ", MGD(θinit, rob)[1][1:3,4], "\n")
+Ta, MTa = MGD(θinit, rob)
+Aa = Ta[1:3,1:3]
+Pa = Ta[1:3,4]
+print("Robot en position : ", Pa, "\n")
 sleep(2)
 
-q_collect = inverseKinematics(θinit, pB, rob, dP, dt)
+#q_collect = inverseKinematicsPos(θinit, Pb, rob, dP, dt)
+
+q_collect, epsilon0_collect = inverseKinematics(θinit, Pb, Ab, rob, dP, dω, dt)
 
 stopsimulation(clientID,simx_opmode_oneshot) # Arrêt de la simulation
+
 
 #=
 # --- Afficher la trajectoire des articulations ---
@@ -131,6 +197,7 @@ Plots.plot(iterations, q_matrix', label=["q1" "q2" "q3" "q4" "q5" "q6" "q7"],
      title="Trajectoire des articulations", legend=:topright)
 =#
 # --- Afficher la trajectoire de l'effecteur ---
+#=
 # Récupérer la position de l'effecteur pour chaque configuration
 trajectoire_effecteur = []
 for q in q_collect
@@ -153,3 +220,21 @@ Plots.plot(iterations_eff, [x_traj y_traj z_traj],
      label=["x" "y" "z"],
      xlabel="Itération", ylabel="Position (m)", 
      title="Trajectoire de l'effecteur selon les axes", legend=:topright)
+=#
+
+# --- Afficher la trajectoire de ϵ0 (erreur d'orientation) ---
+# Convertir epsilon0_collect (Vector de vecteurs) en matrice (3, N_iterations)
+epsilon0_matrix = hcat(epsilon0_collect...)
+
+# Extraire les composantes
+epsilon0_x = epsilon0_matrix[1, :]
+epsilon0_y = epsilon0_matrix[2, :]
+epsilon0_z = epsilon0_matrix[3, :]
+
+iterations_eps = 1:length(epsilon0_x)
+
+# Plot des trajectoires de ϵ0
+Plots.plot(iterations_eps, [epsilon0_x epsilon0_y epsilon0_z], 
+     label=["ϵ0_x" "ϵ0_y" "ϵ0_z"],
+     xlabel="Itération", ylabel="ϵ0 (rad)", 
+     title="Trajectoire du vecteur erreur d'orientation ϵ0", legend=:topright)
